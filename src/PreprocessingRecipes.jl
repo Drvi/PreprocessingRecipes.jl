fe0a0695f22ec9c2cb3c3a76de5d6643d8d05891
Drvi <tomas.drvostep@gmail.com>
2018-12-19 10:29:43 +0100

    Add Structor

--- src/PreprocessingRecipes.jl
+++ src/PreprocessingRecipes.jl
@@ -3,14 +3,14 @@ module PreprocessingRecipes
     using Statistics
     using Dates
 
-    #using Optim: minimizer, optimize
+    using Optim: minimizer, optimize
     #using MultivariateStats
     #using MixedModels
     #using NearestNeighbors
 
     export recipe, add_roles,
            step_standardize!, step_selection!, step_scale!, step_center!, step_closure!,
-           step_function!, step_powertransform!, step_indexer!
+           step_function!, step_powertransform!, step_enumerate!, step_struct!
 
     include("utils.jl")
     include("Roles.jl")
@@ -22,7 +22,8 @@ module PreprocessingRecipes
     include("recipes/Selector.jl")
     include("recipes/Standardizer.jl")
     include("recipes/Closurer.jl")
-    include("recipes/Indexer.jl")
-    #include("recipes/PowerTransformer.jl")
+    include("recipes/Enumerator.jl")
+    include("recipes/PowerTransformer.jl")
+    include("recipes/Structor.jl")
 
 end
